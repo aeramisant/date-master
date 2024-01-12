965a45a52f1b8aa36b1c39b49b267b59270d1ab2
Alex Eramisant <eramisant@yahoo.com>
2024-01-12 10:17:09 +0200

    jacked up and good to go

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testEnvironment: 'node',
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
+};
---
+++ test.js
@@ -0,0 +1,3 @@
+test('adds 1 to 2 should be equal to 3', () => {
+  expect(1 + 2).toBe(3);
+});
